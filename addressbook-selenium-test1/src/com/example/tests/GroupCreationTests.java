571e661b9af61f154a09bf762f58f7c361c80062
Chegodaeva <Annet8031@mail.ru>
2015-11-07 14:20:07 +0300

    ДЗ35
    Загрузка списка групп, проверка что при создании, удалении и модификации
    групп, список корректно изменяется

--- addressbook-selenium-test1/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-test1/src/com/example/tests/GroupCreationTests.java
@@ -1,28 +1,36 @@
 package com.example.tests;
-
+import java.util.Collections;
+import java.util.List;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
+
+
 public class GroupCreationTests extends TestBase{
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
+	
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testGroupCreationWithValidData(GroupData group) throws Exception {
 	app.getNavigationHelper().openMainPage();
 	app.getNavigationHelper().gotoGroupsPage();
+	//save old state
+	List<GroupData> oldList = app.getGroupHelper().getGroups();
+	
+	//action
 	app.getGroupHelper().initGroupCreation();
-	GroupData group = new GroupData();
-	group.name = "groupname 1";
-	group.header = "header 1";
-	group.footer = "foooter 1";
 	app.getGroupHelper().fillGroupForm(group);
 	app.getGroupHelper().submitGroupCreation();
 	app.getGroupHelper().returnToGroupsPage();
+	
+	//save new state
+	
+	List<GroupData> newList= app.getGroupHelper().getGroups();
+	
+	//compare state
+	//assertEquals(newList.size(), oldList.size() + 1) ;
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);
   }
-  @Test
-  public void testEmptyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
-		app.getGroupHelper().submitGroupCreation();
-		app.getGroupHelper().returnToGroupsPage();
-	  }
+  
+ 
 }
